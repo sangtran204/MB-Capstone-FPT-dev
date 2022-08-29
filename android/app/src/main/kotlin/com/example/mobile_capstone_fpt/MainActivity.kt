9c22762354e65ad8ac15225fdafd9aa76f509b7c
tranmausa <currykobejames@gmail.com>
2022-08-29 21:52:28 +0700

    setUp_folder_Mobile

---
+++ android/app/src/main/kotlin/com/example/mobile_capstone_fpt/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobile_capstone_fpt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/app_color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const kBackgroundColor = Color(0xFFFEFEFE);
+const kTitleTextColor = Color(0xFF303030);
+const kBodyTextColor = Color(0xFF4B4B4B);
